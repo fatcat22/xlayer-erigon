2a95a05620dc2e38513e1e65c8a0fd094e21d799
jianguo <zjg555543@163.com>
2025-05-29 18:44:37 +0800

    update

--- test-mock/3-upgrade-true.sh
+++ test-mock/3-upgrade-true.sh
@@ -18,9 +18,9 @@ ROOT_DIR="$(dirname "$PWD_DIR")"
 
 CONTRACT_JSON="artifacts/contracts/verifiers/FflonkVerifier_13.sol/FflonkVerifier_13.json"
 
-docker compose -f docker-compose.yml down xlayer-seqs
+docker stop xlayer-seqs
 sleep 30
-docker compose -f docker-compose.yml down xlayer-agg
+docker stop xlayer-agg
 
 sed_inplace() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
