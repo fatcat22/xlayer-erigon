7b3683459af1a07dba27c8d098a12855ee5677f7
louis.liu <louis.liu@okg.com>
2025-06-02 19:17:37 +0800

    Use a more graceful approach to reset the SMT’s cache.

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -112,17 +112,6 @@ func SpawnSequencingStage(
 			// enable split smt db
 			_ = s.FlushSmtCache(cfg.zk.XLayer.StandaloneSMTDatabase, false)
 		}()
-	} else {
-		// For X Layer, split db and ac
-		if !cfg.zk.XLayer.EnableAsyncCommit {
-			return err
-		}
-
-		s.FlushSmtCacheSignalInc()
-		go func() {
-			defer s.FlushSmtCacheDone()
-			s.ResetCurrentBatchCache(s.BlockNumber)
-		}()
 	}
 
 	return err
@@ -155,7 +144,26 @@ func sequencingBatchStep(
 	if err != nil {
 		return err
 	}
-	defer sdb.Rollback()
+	defer func() {
+		sdb.Rollback()
+
+		if err != nil {
+			if !cfg.zk.XLayer.EnableAsyncCommit {
+				return
+			}
+
+			executionAt, _ := s.ExecutionAt(sdb.tx)
+			if err != nil {
+				return
+			}
+
+			s.FlushSmtCacheSignalInc()
+			go func() {
+				defer s.FlushSmtCacheDone()
+				s.ResetCurrentBatchCache(executionAt + 1)
+			}()
+		}
+	}()
 
 	if sdb.supportAC {
 		// For X Layer, split db and ac
@@ -225,7 +233,7 @@ func sequencingBatchStep(
 				log.Error(fmt.Sprintf("[%s] Failed to get smt max block number", logPrefix), "error", err, "smtMaxBlockNumber", smtMaxBlockNumber)
 				return err
 			}
-			if smtMaxBlockNumber != 0 && smtMaxBlockNumber+1 <= executionAt {
+			if smtMaxBlockNumber != 0 && smtMaxBlockNumber+1 < executionAt {
 				targetBlock, err := getTargetBlockForSMTAlignment(sdb, logPrefix, executionAt, smtMaxBlockNumber)
 				if err != nil {
 					return err
