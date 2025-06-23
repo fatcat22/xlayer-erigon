534fed937a00907b3fa60f439fc556fbfd12c8a0
zhe.yang <pandayt@163.com>
2025-06-23 18:49:06 +0800

    fix errors

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -2395,7 +2395,6 @@ func SetEthConfig(ctx *cli.Context, nodeConfig *nodecfg.Config, cfg *ethconfig.C
 
 	// For X Layer
 	SetBulkAddTxs(ctx, cfg)
-	SetVerificationConfigs(ctx, cfg)
 }
 
 // SetDNSDiscoveryDefaults configures DNS discovery with the given URL if
